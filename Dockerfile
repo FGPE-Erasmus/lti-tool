369fc4569eb9e838a8e9dce494165a5d12da737f
José Carlos Paiva <josepaiva94@gmail.com>
2021-11-12 14:42:56 +0000

    finalize dockerization

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,11 @@ FROM node:lts-alpine AS builder
 ARG NODE_ENV=default
 ENV NODE_ENV=${NODE_ENV}
 
-RUN apk add --no-cache python make g++
+WORKDIR /usr/app
+
+RUN apk add --no-cache py-pip make g++
+
+COPY package*.json ./
 
 RUN npm install
 
@@ -15,6 +19,12 @@ FROM node:lts-alpine
 ARG NODE_ENV=default
 ENV NODE_ENV=${NODE_ENV}
 
+WORKDIR /usr/app
+
 RUN apk add --no-cache bash coreutils
+
+COPY --from=builder /usr/app/node_modules node_modules
+
+COPY . .
 
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const routes = require('./src/routes')
 const lti = require('ltijs').Provider
 
 // Setup
-lti.setup('EXAMPLEKEY',
-  {
-    url: 'mongodb://127.0.0.1:27017'
-  }, {
-    staticPath: path.join(__dirname, './public'), // Path to static files
-    cookies: {
-      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
-      sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
-    },
-    devMode: true // Set DevMode to true if the testing platform is in a different domain and https is not being used
-  })
+lti.setup(process.env.LTI_TOOL_KEY, {
+  url: `mongodb://${process.env.LTI_TOOL_DB_HOST}:${process.env.LTI_TOOL_DB_PORT}/${process.env.LTI_TOOL_DB_NAME}`,
+  connection: {
+    authSource: process.env.LTI_TOOL_DB_AUTH,
+    user: process.env.LTI_TOOL_DB_USERNAME,
+    pass: process.env.LTI_TOOL_DB_PASSWORD
+  }
+},
+{
+  staticPath: path.join(__dirname, './public'), // Path to static files
+  cookies: {
+    secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
+    sameSite: '' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+  },
+  devMode: true // Set DevMode to true if the testing platform is in a different domain and https is not being used
+});
 
 // When receiving successful LTI launch redirects to app
 lti.onConnect(async (token, req, res) => {
@@ -32,19 +37,27 @@ lti.app.use(routes)
 
 // Setup function
 const setup = async () => {
-  await lti.deploy({ port: process.env.PORT })
+  console.log(process.env);
+  await lti.deploy({ port: process.env.LTI_TOOL_PORT })
 
   /**
    * Register platform
    */
-await lti.registerPlatform({
-    url: 'https://localhost',
-    name: 'Moodle',
-    clientId: 'ztXWfbnU3uTAP95',
-    authenticationEndpoint: 'https://localhost/mod/lti/auth.php',
-    accesstokenEndpoint: 'https://localhost/mod/lti/token.php',
-    authConfig: { method: 'JWK_SET', key: 'https://localhost/mod/lti/certs.php' }
+  const platform = await lti.registerPlatform({
+    url: process.env.MOODLE_URL,
+    name: process.env.LTI_TOOL_NAME,
+    clientId: process.env.LTI_TOOL_CLIENT_ID,
+    authenticationEndpoint: `${process.env.MOODLE_URL}/mod/lti/auth.php`,
+    accesstokenEndpoint: `${process.env.MOODLE_URL}/mod/lti/token.php`,
+    authConfig: {
+      method: 'JWK_SET',
+      key: `${process.env.MOODLE_URL}/mod/lti/certs.php`
+    }
   }) 
+
+  const authConfig = await platform.platformAuthConfig();
+  console.log(authConfig);
+  console.log(await platform.platformPublicKey())
 }
 
 setup()
